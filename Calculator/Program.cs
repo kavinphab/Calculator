0212da561826a2e7b4668e7cf5b4677d657f072d
Kavin Phabiani <Kavin.Phabiani@GMR.local>
2019-01-20 12:29:48 -0800

    initial commit of calculator project

---
+++ Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static int add(int[] numbers)
+        {
+          
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                int answer = 0;
+                numbers[i] += answer;
+                return answer;
+            }
+           
+        }
+
+        static int max(int [] numbers)
+        {
+
+           int max = numbers.Max();
+            return max;
+            
+        }
+        static void Main(string[] args)
+        {
+            // create a calculator with functions to add, subtract, multiply, divide, square, square root, find average, find min, find max, sum
+
+
+        }
+    }
+}
